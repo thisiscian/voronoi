48d302119adf3db9903c93f9f3da5b2452ab6a36
hitcherland <hitcherland@gmail.com>
2019-04-25 21:31:35 +0100

    added dynamic point count

--- voronoi.py
+++ voronoi.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 
+import click
 import matplotlib
 matplotlib.use('GTK3Agg')
 import matplotlib.pyplot as plt
 
 import pyvoronoi as voronoi
 
-solution = voronoi.solve(4)
+@click.command()
+@click.argument("count", default=3)
+def main(count):
+    solution = voronoi.solve(count)
 
-for x_data, y_data in solution['edges']:
-    plt.plot(x_data, y_data, '-')
+    for x_data, y_data in solution['edges']:
+        plt.plot(x_data, y_data, '-')
 
-for x_data, y_data in solution['unsolved']:
-    plt.plot(x_data, y_data, '--')
+    for x_data, y_data in solution['unsolved']:
+        plt.plot(x_data, y_data, '--')
 
-plt.scatter(*solution['points'], marker=".")
+    plt.scatter(*solution['points'], marker=".")
 
-plt.axis("off")
-plt.axis("equal")
-plt.axis([0,1,0,1])
-plt.show()
+    plt.axis("off")
+    plt.axis("equal")
+    plt.axis([0,1,0,1])
+    plt.show()
+
+if __name__ == '__main__':
+    main();
